14a961a9425c6404ea5ab892ed31ff0a0b260a97
Mehul <cs20077@glbitm.ac.in>
2023-07-28 18:58:13 +0530

    updated the about page

--- src/componenets/About.js
+++ src/componenets/About.js
@@ -13,7 +13,7 @@ export default function About() {
     }
   }, []);
   function open(e) {
-    const btns = document.querySelectorAll(".about-me button")
+    const btns = document.querySelectorAll(".wrap-container button")
     const cons = document.querySelectorAll(".about-container")
     btns.forEach(btn => {
       btn.classList.remove('active')
@@ -30,24 +30,42 @@ export default function About() {
         <div className="intro">
           <Lottie animationData={animationData} className='ani-1' />
           <div className="wrap-container">
-            <div className="about-me">
-              <button onClick={() => open(0)} className="btn active">
-                Skills
-              </button>
-              <button onClick={() => open(1)} className="btn">
-                Resume
-              </button>
-            </div>
+            <button onClick={() => open(0)} className="btn active">Skills</button>
+            <button onClick={() => open(1)} className="btn">Resume</button>
+
             <div className="about-container skills open">
-              <h2>skills</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam asperiores, sint ea, reiciendis nulla molestiae nam quas corrupti est cupiditate vel fuga. Est quo in, adipisci praesentium quae quos totam.
-              </p>
+              <div className="skill-container dev">
+                <div className="skill-container-title">Development</div>
+                <div className="dev-item-container">
+                  <div className="dev-item">HTML</div>
+                  <div className="dev-item">Python</div>
+                  <div className="dev-item">Js</div>
+                  <div className="dev-item">C++</div>
+                  <div className="dev-item">C</div>
+                  <div className="dev-item">MySql</div>
+                  <div className="dev-item">MongoDb</div>
+                  <div className="dev-item">Reactjs</div>
+                  <div className="dev-item">Express</div>
+                  <div className="dev-item">NodeJs</div>
+                  <div className="dev-item">Css </div>
+                  <div className="dev-item">UI/UX</div>
+                  <div className="dev-item">Backend</div>
+                </div>
+              </div>
+              <div className="skill-container plat">
+                <div className="skill-container-title">Platforms</div>
+                <ul className="plat-items">
+                  <li className="plat-item"><a href="https://leetcode.com/mehul101/" target='_blank' rel='noreferrer'>Leetcode</a></li>
+                  <li className="plat-item"><a href="https://www.cloudskillsboost.google/public_profiles/a4d845ab-ec5d-46df-a554-e86cf9d08b05" target='_blank' rel='noreferrer'>Google Cloud</a></li>
+                  <li className="plat-item"><a href='https://github.com/mehulrana5' target='_blank' rel='noreferrer'>Github</a></li>
+                  <li className="plat-item"><a href='https://www.linkedin.com/in/mehul-rana-463aa0227/' target='_blank' rel='noreferrer'>LinkedIn</a></li>
+                </ul>
+              </div>
             </div>
             <div className="about-container resume">
-                <a href={resume}  rel="noreferrer" target ='_blank'>
-                  <div className="resume-img-mask"></div>
-                </a>
+              <a href={resume} rel="noreferrer" target='_blank'>
+                <div className="resume-img-mask"></div>
+              </a>
             </div>
           </div>
         </div>
--- src/componenets/Home.js
+++ src/componenets/Home.js
@@ -1,13 +1,16 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './css/home.css';
 import mycontext from '../MyContext';
+import Lottie from "lottie-react";
+import animationData from "./animations/down-arrow-ani2.json"
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   const context = useContext(mycontext);
+  const [key, setKey] = useState(0);
 
   const handleIntersection = (entries) => {
     entries.forEach((entry) => {
@@ -21,7 +24,9 @@ export default function Home() {
       }
     });
   };
+
   useEffect(() => {
+    // Smooth scroll snapping using gsap
     gsap.to('.background', {
       y: '-8%',
       ease: 'none',
@@ -32,8 +37,26 @@ export default function Home() {
         scrub: 1,
       },
     });
-    console.log("navBtn:"+context.navBtn);
-    if(!context.navBtn){
+
+    // Event listener for the down arrow key
+    const downArrowHandler = (event) => {
+      if (event.key === 'ArrowDown') {
+        setKey(1);
+      }
+    };
+
+    if (!key) {
+      window.addEventListener('keydown', downArrowHandler);
+    }
+
+    // Cleanup for event listener
+    return () => {
+      window.removeEventListener('keydown', downArrowHandler);
+    };
+  }, [key]);
+
+  useEffect(() => {
+    if (!context.navBtn) {
       const observer = new IntersectionObserver(handleIntersection, {
         root: null,
         threshold: 0.5,
@@ -44,14 +67,20 @@ export default function Home() {
       });
       return () => observer.disconnect();
     }
+
     setTimeout(() => {
       context.setNavBtn(0);
     }, 1000);
-
+    // eslint-disable-next-line
   }, [context.navBtn]);
+
   return (
     <>
       <div className="home-page pages">
+        <div className="up-down-key">
+          Press key
+          <Lottie loop={!key} animationData={animationData} className='ani-2' />
+        </div>
         <div className="img1"></div>
         <div className="home-container">
           Hi, i am <span className="my-name">Mehul.</span>
