2ba48e60aa2296c6e92ed4a4210d9a37b4f7d24f
Mehul <cs20077@glbitm.ac.in>
2023-08-09 01:04:21 +0530

    modified the project section

--- src/componenets/Project.js
+++ src/componenets/Project.js
@@ -1,28 +1,149 @@
 import React from 'react'
 import "./css/project.css"
+import noImg from './img/no-img.jpg'
+import proj1_img from './img/proj1-img1.png'
 
 export default function Project() {
   return (
     <div className="show-case pages">
       <div className="card-container">
-        <div className="card">
-          <div className="proj-img">
-            <div className="proj-img-container p1"></div>
+        <div className="card-container-scroller">
+          <div className="card">
+            <div className="proj-img">
+              <img className='proj-img-container' src={proj1_img} alt={noImg} srcset="" />
+            </div>
+            <div className="content">
+              <div className="proj-title">
+                Cognise
+              </div>
+              <div className="proj-descp">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati asperiores ratione corrupti veritatis placeat. Esse odio cumque, officiis sequi aspernatur fugit beatae fuga? Impedit asperiores quae aliquid optio tempore nulla!
+              </div>
+              <div className="proj-tags">
+                <div className="tag">Reactjs</div>
+                <div className="tag">MongoDb</div>
+                <div className="tag">Chat Gpt</div>
+              </div>
+              <button className="card-btn">
+                view project
+              </button>
+            </div>
           </div>
-          <div className="proj-title">
-            Cognise App
+        </div>
+        <div className="card-container-scroller">
+          <div className="card">
+            <div className="proj-img">
+              <img className='proj-img-container' src={noImg} alt="" srcset="" />
+            </div>
+            <div className="content">
+              <div className="proj-title">
+                [Project name]
+              </div>
+              <div className="proj-descp">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati asperiores ratione corrupti veritatis placeat. Esse odio cumque, officiis sequi aspernatur fugit beatae fuga? Impedit asperiores quae aliquid optio tempore nulla!
+              </div>
+              <div className="proj-tags">
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+              </div>
+              <button className="card-btn">
+                view project
+              </button>
+            </div>
           </div>
-          <div className="proj-tags">
-            <div className="tag">Reactjs</div>
-            <div className="tag">MongoDb</div>
-            <div className="tag">Chat Gpt</div>
+        </div>
+        <div className="card-container-scroller">
+          <div className="card">
+            <div className="proj-img">
+              <img className='proj-img-container' src={noImg} alt="" srcset="" />
+            </div>
+            <div className="content">
+              <div className="proj-title">
+                [Project name]
+              </div>
+              <div className="proj-descp">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati asperiores ratione corrupti veritatis placeat. Esse odio cumque, officiis sequi aspernatur fugit beatae fuga? Impedit asperiores quae aliquid optio tempore nulla!
+              </div>
+              <div className="proj-tags">
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+              </div>
+              <button className="card-btn">
+                view project
+              </button>
+            </div>
           </div>
-          <div className="proj-descp">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati asperiores ratione corrupti veritatis placeat. Esse odio cumque, officiis sequi aspernatur fugit beatae fuga? Impedit asperiores quae aliquid optio tempore nulla!
+        </div>
+        <div className="card-container-scroller">
+          <div className="card">
+            <div className="proj-img">
+              <img className='proj-img-container' src={noImg} alt="" srcset="" />
+            </div>
+            <div className="content">
+              <div className="proj-title">
+                [Project name]
+              </div>
+              <div className="proj-descp">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati asperiores ratione corrupti veritatis placeat. Esse odio cumque, officiis sequi aspernatur fugit beatae fuga? Impedit asperiores quae aliquid optio tempore nulla!
+              </div>
+              <div className="proj-tags">
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+              </div>
+              <button className="card-btn">
+                view project
+              </button>
+            </div>
           </div>
-          <button className="card-btn">
-            view project
-          </button>
+        </div>
+        <div className="card-container-scroller">
+          <div className="card">
+            <div className="proj-img">
+              <img className='proj-img-container' src={noImg} alt="" srcset="" />
+            </div>
+            <div className="content">
+              <div className="proj-title">
+                [Project name]
+              </div>
+              <div className="proj-descp">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati asperiores ratione corrupti veritatis placeat. Esse odio cumque, officiis sequi aspernatur fugit beatae fuga? Impedit asperiores quae aliquid optio tempore nulla!
+              </div>
+              <div className="proj-tags">
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+              </div>
+              <button className="card-btn">
+                view project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-container-scroller">
+          <div className="card">
+            <div className="proj-img">
+              <img className='proj-img-container' src={noImg} alt="" srcset="" />
+            </div>
+            <div className="content">
+              <div className="proj-title">
+                [Project name]
+              </div>
+              <div className="proj-descp">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati asperiores ratione corrupti veritatis placeat. Esse odio cumque, officiis sequi aspernatur fugit beatae fuga? Impedit asperiores quae aliquid optio tempore nulla!
+              </div>
+              <div className="proj-tags">
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+                <div className="tag">[Project tags]</div>
+              </div>
+              <button className="card-btn">
+                view project
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       <h3 className='show-case-footer'>
